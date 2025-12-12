a44c49fe114851b967df0308d02f501191aa3217
Fredika Putra <53744519+fredikaputra@users.noreply.github.com>
2025-12-12 17:47:36 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -2,9 +2,15 @@ pnpx create-next-app@latest web
 cd web
 pnpm create playwright
 pnpm install antd @ant-design/nextjs-registry
-pnpm add --save-dev prettier
+pnpm add --save-dev prettier vitest @vitejs/plugin-react jsdom @testing-library/react @testing-library/dom vite-tsconfig-paths @vitest/coverage-v8
 cd ..
 cp -r web/. .
 cp -r my-next-setup/src/. .
-rm -rf .idx web my-next-setup pnpm-lock.yaml README.md
+rm -rf .idx web my-next-setup pnpm-lock.yaml README.md app/page.module.css
 git add .
+
+read -p 'Enter to setup Nextjs MCP...'
+echo '' >> .gitignore
+echo '.idx' >> .gitignore
+echo 'opencode.jsonc' >> .gitignore
+pnpm add --save-dev next-devtools-mcp
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
